c8a210b764f1527ce589d886cbffe9b328af560f
Arndt <leininger@politik.uni-mainz.de>
2017-10-10 22:59:11 +0200

    Kapitel 1 und 2 mit Blocks

--- compile.R
+++ compile.R
@@ -1,10 +1,7 @@
 # gitbook
-
 bookdown::render_book("index.Rmd", output_dir = "docs")
 
 # PDF
-
-
 bookdown::render_book("index.Rmd", output_dir = "docs", 
 	output_format = bookdown::pdf_book(),
 	params = list(emoji = FALSE))
