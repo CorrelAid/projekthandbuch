813c8196b53e992959329cce238f6da8bf0f0a2e
Unknown <friedrike.preu@uni-konstanz.de>
2017-10-11 18:37:46 +0200

    comment out problematic configs...

--- compile.R
+++ compile.R
@@ -1,7 +1,7 @@
-# gitbook
 bookdown::render_book("index.Rmd", output_dir = "docs")
+# gitbook
 
 # PDF
-bookdown::render_book("index.Rmd", output_dir = "docs", 
+bookdown::render_book("index.Rmd", output_dir = "docs",
 	output_format = bookdown::pdf_book(),
 	params = list(emoji = FALSE))
